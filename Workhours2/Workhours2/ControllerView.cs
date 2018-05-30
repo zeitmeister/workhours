7872968dd5f8d13a54a1d6ac81da8eb0811daac2
zeitmeister <simon.sporrong@hotmail.se>
2018-05-30 22:52:13 +0200

    lägger till

---
+++ Workhours2/Workhours2/ControllerView.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Workhours2
+{
+    public partial class ControllerView : UIView
+    {
+        public ControllerView (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
